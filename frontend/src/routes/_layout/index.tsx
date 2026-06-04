dea882a16fc683dbeb9fd8d43d317715a7858ede
ryanyang0818 <sheephead0818@gmail.com>
2026-06-04 23:59:05 +0800

    :)

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -20,7 +20,7 @@ function Dashboard() {
     <div>
       <div>
         <h1 className="text-2xl truncate max-w-sm">
-          Hi, {currentUser?.full_name || currentUser?.email} 👋
+          Hello, {currentUser?.full_name || currentUser?.email} 👋
         </h1>
         <p className="text-muted-foreground">
           Welcome back, nice to see you again!!!
